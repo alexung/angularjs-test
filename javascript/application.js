2f461db8f417d8f961056d89b483c56da9a91331
Alex Ung <alexung.ucsb@gmail.com>
2015-02-07 09:09:07 -0800

    made 2 views, they connect with one another, can add family members but there is no persistence once you refresh

--- javascript/application.js
+++ javascript/application.js
@@ -1,3 +1,7 @@
+     //   {name: 'Alex Ung', profession: 'Software Engineer at Accenture'},
+     // {name: 'Derrick Ung', profession: 'Economic Analyst at the County of Riverside'},
+     //  {name: 'Nareth Ung', profession: 'Network Engineer at Sempra Energies'},
+     //  {name: 'Vann Heng', profession: 'Materials Science Engineer at Boeing'}
   var myApp = angular.module('myApp', ['ngRoute']);
 
   myApp.config(function($routeProvider){
@@ -15,15 +19,32 @@
       .otherwise({ redirectTo: '/' });
   });
 
-
-    myApp.controller('SimpleController', function($scope){
-
-      $scope.family = [
-     {name: 'Alex Ung', profession: 'Software Engineer at Accenture'},
-     {name: 'Derrick Ung', profession: 'Economic Analyst at the County of Riverside'},
+  myApp.factory('simpleFactory', function(){
+    var family = [
+      {name: 'Alex Ung', profession: 'Software Engineer at Accenture'},
+      {name: 'Derrick Ung', profession: 'Economic Analyst at the County of Riverside'},
       {name: 'Nareth Ung', profession: 'Network Engineer at Sempra Energies'},
       {name: 'Vann Heng', profession: 'Materials Science Engineer at Boeing'}
-      ];
+    ];
+
+    var factory = {};
+
+    factory.getFamilyMembers = function() {
+      return family;
+    };
+
+    return factory;
+  });
+
+    myApp.controller('SimpleController', function($scope, simpleFactory){
+
+      $scope.family = [];
+
+      init();
+
+      function init(){
+        $scope.family = simpleFactory.getFamilyMembers();
+      };
 
       $scope.addFamilyMember = function(){
         $scope.family.push({
