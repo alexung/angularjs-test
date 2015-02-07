89ae9875699e97e53c69350a0dc5df41bf7c57a5
Alex Ung <alexung.ucsb@gmail.com>
2015-02-06 22:11:59 -0800

    separated the controller from the module

--- javascript/application.js
+++ javascript/application.js
@@ -1,11 +1,13 @@
-  var myApp = angular.module('myApp', []).controller('SimpleController', function($scope){
+  var myApp = angular.module('myApp', [])
+
+    myApp.controller('SimpleController', function($scope){
 
       $scope.family = [
      {name: 'Alex Ung', profession: 'Software Engineer at Accenture'},
      {name: 'Derrick Ung', profession: 'Economic Analyst at the County of Riverside'},
       {name: 'Nareth Ung', profession: 'Network Engineer at Sempra Energies'},
       {name: 'Vann Heng', profession: 'Materials Science Engineer at Boeing'}
-  ];
+      ];
 
     });
 
