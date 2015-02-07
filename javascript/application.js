10cb0e2c01fc56a0d5447cdd4d659570e0af3606
Alex Ung <alexung.ucsb@gmail.com>
2015-02-06 18:33:48 -0800

    separated out the angular module into a javascript file

--- javascript/application.js
+++ javascript/application.js
@@ -1,11 +1,13 @@
-$(document).ready(function(){
-  SimpleController($scope);
-});
+  var myApp = angular.module('myApp', []).controller('SimpleController', function($scope){
 
-function SimpleController($scope){
-
-  $scope.family = [
-    {name: 'Alex Ung', profession: 'Software Engineer at Accenture'}, {name: 'Derrick Ung', profession: 'Economic Analyst at the County of Riverside'}, {name: 'Nareth Ung', profession: 'Network Engineer at Sempra Energies'}, {name: 'Vann Heng', profession: 'Materials Science Engineer at Boeing'}
+      $scope.family = [
+     {name: 'Alex Ung', profession: 'Software Engineer at Accenture'},
+     {name: 'Derrick Ung', profession: 'Economic Analyst at the County of Riverside'},
+      {name: 'Nareth Ung', profession: 'Network Engineer at Sempra Energies'},
+      {name: 'Vann Heng', profession: 'Materials Science Engineer at Boeing'}
   ];
 
-}
+    });
+
+
+
